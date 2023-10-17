941cf983bc8d2258977e6cee577cb7e5492905da
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2023-10-16 21:33:42 -0700

    full crud working

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Add task to databse
     path('addTask/', views.addTask, name="addTask"),
+    # Mark a task as completed
+    path('markAsCompleted/<int:pk>/', views.markAsCompleted, name="markAsCompleted"),
+    # Mark a task as incompleted
+    path('markAsIncompleted/<int:pk>/', views.markAsIncompleted, name="markAsIncompleted"),
+    # Edit a task 
+    path('editTask/<int:pk>/', views.editTask, name="editTask"),
+    # Delete a task
+    path('deleteTask/<int:pk>/', views.deleteTask, name="deleteTask"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import redirect, render, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
 from .models import Task
 
 # Create your views here.
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('/')
+
+# Mark a task as completed
+def markAsCompleted(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('/')
+
+# Mark a task as incompleted
+def markAsIncompleted(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('/')
+
+# Edit a task
+def editTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.task = request.POST['task']
+        task.save()
+        return redirect('/')
+    else:
+        context = {
+            'task': task
+        }
+        return render(request, 'editTask.html', context)
+
+# Delete a task
+def deleteTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
     return redirect('/')
